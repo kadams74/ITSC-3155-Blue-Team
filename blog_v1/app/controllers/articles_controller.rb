5f65c858b1b48c34f67e7b3eecd822afc457d740
hbrown61 <hbrown61@uncc.edu>
2020-04-02 02:44:33 +0000

    Updated it to current lab. Problems with deleting article that has comments.

--- blog_v1/app/controllers/articles_controller.rb
+++ blog_v1/app/controllers/articles_controller.rb
@@ -13,13 +13,36 @@ class ArticlesController < ApplicationController
     
     def create
         @article = Article.new(article_params)
+        #byebug
         
         if @article.save
+            #byebug
             redirect_to @article
         else
             render 'new'
         end
     end
+    
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+    
+    def destroy
+        # byebug
+        @article = Article.find(params[:id])
+        @article.destroy
+        # byebug
+        redirect_to articles_path
+    end 
 end
 
 private
--- blog_v1/config/routes.rb
+++ blog_v1/config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   get 'movies/index'
   get 'welcome/index'
   
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
