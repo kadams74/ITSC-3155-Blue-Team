21a1c1e107eb29bb709c52e2ec3b0e46ca2b5890
Kevin Travis <ktravis8@uncc.edu>
2020-04-09 17:42:33 +0000

    Change My Blog to Articles

--- blog_v1/features/step_definitions/article_steps.rb
+++ blog_v1/features/step_definitions/article_steps.rb
@@ -10,6 +10,9 @@ Then("I should be on the {string} page") do |string|
 expect(page).to have_content(string)
 end
 
+When("I click on the {string} linkThen I should be on the {string} page") do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
 
 When("I should see the {string} field") do |string|
 expect(page).to have_field(string)
