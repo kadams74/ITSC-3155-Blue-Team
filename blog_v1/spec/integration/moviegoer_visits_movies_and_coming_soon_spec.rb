5c4c491819b2f9446d29490cdeca272d7586022f
Kyril <kadams74@uncc.edu>
2020-04-10 17:58:54 +0000

    added moviegoer visits movies scenario 2

--- blog_v1/spec/integration/moviegoer_visits_movies_and_coming_soon_spec.rb
+++ blog_v1/spec/integration/moviegoer_visits_movies_and_coming_soon_spec.rb
@@ -12,4 +12,12 @@ feature "Moviegoer visits Movies page, and then visits Coming Soon page" do
        expect(page).to have_content("Netflix")
    end
    
+   scenario "Moviegoer succesfully navigates to the coming soon page from the mavies page via the navigation bar." do
+      visit movies_index_path
+      expect(page).to have_content("Movies")
+      expect(page).to have_content("In Theaters")
+      expect(page).to have_content("Netflix")
+      click_link "Coming Soon"
+      expect(page).to have_content("Coming Soon")
+   end
 end
