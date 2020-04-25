f6c87e4991a886b13b30e94556e9d788329924f5
Corey Schneider <cschne11@uncc.edu>
2020-04-25 01:19:58 +0000

    Added search functionality with article titles only

--- blog_v1/config/routes.rb
+++ blog_v1/config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
+  get 'search', to: 'search#search', as: 'search_page'
   
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
