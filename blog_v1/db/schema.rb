ba7cc942cad08df4b4a0e6cfb297146075fb173a
Kyril <kadams74@uncc.edu>
2020-05-05 03:25:41 +0000

    Regenerated ratings migration

--- blog_v1/db/schema.rb
+++ blog_v1/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_27_035728) do
+ActiveRecord::Schema.define(version: 2020_05_05_032223) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
