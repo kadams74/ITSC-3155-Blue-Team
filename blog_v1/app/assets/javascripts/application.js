4dc5dfbd3a83a4773155188efd0ecfba78cd7d98
Corey Schneider <cschne11@uncc.edu>
2020-05-05 01:59:43 +0000

    Added new font; Added slideshow to home page; Put pages in containers

--- blog_v1/app/assets/javascripts/application.js
+++ blog_v1/app/assets/javascripts/application.js
@@ -15,3 +15,104 @@
 //= require turbolinks
 //= require_tree .
 
+var slideshow = {
+  /* [THE OPTIONS] */
+  delay: 8000, // time in between each slide
+
+  /* [THE MECHANICS] */
+  container: null, // HTML slides container
+  caption: null, // HTML caption container
+  slides: [], // array to hold all the slide data
+  current: 0, // pointer, the current slide
+  timer: null, // to hold the timer object
+
+  start: function () {
+  // slideshow.start() : start the slideshow
+
+    // GET THE ELEMENTS
+    slideshow.container = document.getElementById("slides");
+    slideshow.caption = document.getElementById("slide-caption");
+
+    // GET ALL SLIDES DATA
+    let all = slideshow.container.getElementsByTagName("img");
+    if (all.length>0) {
+      for (let slide of all) {
+        slideshow.slides.push({
+          src : slide.src,
+          caption : slide.dataset.caption
+        });
+      }
+
+      // ATTACH NEXT/PREVIOUS BUTTONS
+      document.getElementById("slide-left").addEventListener("click", slideshow.prev);
+      document.getElementById("slide-right").addEventListener("click", slideshow.next);
+
+      // START ROLLING
+      slideshow.draw();
+    }
+  },
+
+  draw: function () {
+  // slideshow.draw() : draw current slide + prime timer
+
+    // CREATE SLIDE
+    var next = document.createElement("img");
+    next.src = slideshow.slides[slideshow.current].src;
+    next.classList.add("ninja");
+    slideshow.container.innerHTML = "";
+    slideshow.container.appendChild(next);
+
+    // SILLY BUG. DIRECTY REMOVING THE CLASS WILL NOT FIRE THE CSS ANIMATION
+    // WILL HAVE TO WAIT FOR A SPLIT SECOND TO PROPERLY RUN THE ANIMATION
+    setTimeout(function(){
+      next.classList.remove("ninja");
+    }, 1);
+
+    // SET CAPTION
+    slideshow.caption.innerHTML = slideshow.slides[slideshow.current].caption;
+
+    // SET TIMER FOR NEXT SLIDE
+    slideshow.timer = setTimeout(slideshow.next, slideshow.delay);
+  },
+
+  next: function () {
+  // slideshow.next() : go to next slide
+ 
+    // CLEAR OLD TIMER FIRST
+    if (slideshow.timer != null) {
+      clearTimeout(slideshow.timer);
+      slideshow.timer = null;
+    }
+ 
+    // TO THE NEXT SLIDE
+    slideshow.current++;
+ 
+    // ENDED - ROTATE BACK TO THE FIRST SLIDE
+    if (slideshow.current == slideshow.slides.length) {
+      slideshow.current = 0;
+    }
+    slideshow.draw();
+  },
+
+  prev: function () {
+  // slideshow.prev() : go to last slide
+ 
+    // CLEAR OLD TIMER FIRST
+    if (slideshow.timer != null) {
+      clearTimeout(slideshow.timer);
+      slideshow.timer = null;
+    }
+ 
+    // TO THE NEXT SLIDE
+    slideshow.current--;
+ 
+    // ENDED - ROTATE BACK TO THE FIRST SLIDE
+    if (slideshow.current <0 ) {
+      slideshow.current = slideshow.slides.length - 1;
+    }
+    slideshow.draw();
+  }
+};
+
+// FIRE UP SLIDE SHOW ON PAGE LOAD
+window.addEventListener("load", slideshow.start);
--- blog_v1/db/schema.rb
+++ blog_v1/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_27_035728) do
+ActiveRecord::Schema.define(version: 2020_04_27_015513) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -56,14 +56,6 @@ ActiveRecord::Schema.define(version: 2020_04_27_035728) do
     t.text "synopsis"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "ratings", force: :cascade do |t|
-    t.float "value"
-    t.integer "movie_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["movie_id"], name: "index_ratings_on_movie_id"
   end
 
   create_table "reviews", force: :cascade do |t|
